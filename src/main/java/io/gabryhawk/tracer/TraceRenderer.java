82cfac3e0f196cd1f88db8fab72f8cae76d171b2
Gabriele Stefani <gabristefani@live.it>
2021-01-13 01:43:14 +0100

    Added chest tracer

--- src/main/java/io/gabryhawk/tracer/TraceRenderer.java
+++ src/main/java/io/gabryhawk/tracer/TraceRenderer.java
@@ -2,6 +2,8 @@ package io.gabryhawk.tracer;
 
 import org.lwjgl.opengl.GL11;
 
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.Entity;
@@ -14,6 +16,7 @@ import net.minecraft.util.math.Vec3d;
 public class TraceRenderer {
 
     public static void onRender(float dt) {
+        GL11.glPushMatrix();
 
         MinecraftClient MC = MinecraftClient.getInstance();
 
@@ -35,15 +38,10 @@ public class TraceRenderer {
         GL11.glEnable(GL11.GL_CULL_FACE);
         GL11.glDisable(GL11.GL_DEPTH_TEST);
         GL11.glDisable(GL11.GL_LIGHTING);
-
-        GL11.glPushMatrix();
         RenderUtils.applyRenderOffset();
+        Vec3d start = new Vec3d(f2 * f3, f4, f1 * f3).add(RenderUtils.getCameraPos());
 
         for (Entity e : MC.world.getEntities()) {
-
-            BlockPos pos = e.getBlockPos();
-
-            System.out.println(e.getType());
 
             if (e.getType() == EntityType.PLAYER && e.getUuid() != player.getUuid()) {
 
@@ -51,37 +49,9 @@ public class TraceRenderer {
 
                 float healthPercentage = nearPlayer.getHealth() / nearPlayer.getMaxHealth();
 
-                GL11.glColor4f((1f - healthPercentage), healthPercentage, 0.25f, 0.5f);
+                drawTrace(start, e.getBlockPos(), (1f - healthPercentage), healthPercentage, 0.25f);
+                drawBlock(start, e.getBlockPos(), (1f - healthPercentage), healthPercentage, 0.25f);
 
-                // tracer line
-                GL11.glBegin(GL11.GL_LINES);
-                {
-                    // set start position
-                    Vec3d start = new Vec3d(f2 * f3, f4, f1 * f3).add(RenderUtils.getCameraPos());
-
-                    // set end position
-                    Vec3d end = Vec3d.ofCenter(pos);
-
-                    // draw line
-                    GL11.glVertex3d(start.x, start.y, start.z);
-                    GL11.glVertex3d(end.x, end.y, end.z);
-                }
-                GL11.glEnd();
-
-                // block box
-                {
-                    GL11.glPushMatrix();
-                    GL11.glTranslated(pos.getX(), pos.getY(), pos.getZ());
-
-                    RenderUtils.drawOutlinedBox();
-
-                    GL11.glColor4f((1f - healthPercentage), healthPercentage, 0.25f, 0.25f);
-                    RenderUtils.drawSolidBox();
-
-                    GL11.glPopMatrix();
-                }
-
-                GL11.glEnd();
             }
 
             // System.out.println("Entity: " + e.toString() + " X: " + e.lastRenderX + " Y:
@@ -89,13 +59,38 @@ public class TraceRenderer {
             // + e.lastRenderZ);
         }
 
-        GL11.glPopMatrix();
+        for (BlockEntity be : MC.world.blockEntities) {
+            if (be.getType() == BlockEntityType.CHEST) {
+                drawBlock(start, be.getPos(), 0f, 1f, 1f);
+            }
+        }
 
         // GL resets
         GL11.glEnable(GL11.GL_DEPTH_TEST);
         GL11.glEnable(GL11.GL_TEXTURE_2D);
         GL11.glDisable(GL11.GL_BLEND);
         GL11.glDisable(GL11.GL_LINE_SMOOTH);
+        GL11.glPopMatrix();
+    }
+
+    private static void drawTrace(Vec3d start, BlockPos pos, float red, float green, float blue) {
+        Vec3d end = Vec3d.ofCenter(pos);
+        GL11.glBegin(GL11.GL_LINES);
+        GL11.glColor4f(red, green, blue, 0.5f);
+        GL11.glVertex3d(start.x, start.y, start.z);
+        GL11.glVertex3d(end.x, end.y, end.z);
+        GL11.glEnd();
+    }
+
+    private static void drawBlock(Vec3d start, BlockPos pos, float red, float green, float blue) {
+        GL11.glPushMatrix();
+        GL11.glTranslated(pos.getX(), pos.getY(), pos.getZ());
+        GL11.glColor4f(red, green, blue, 0.5f);
+        RenderUtils.drawOutlinedBox();
+        GL11.glColor4f(red, green, blue, 0.25f);
+        RenderUtils.drawSolidBox();
+        GL11.glPopMatrix();
+
     }
 
 }
